7247317ba7d27258532734de3256a2a627f544fe
matteov021 <95315996+matteov021@users.noreply.github.com>
2024-04-09 16:54:40 -0600

    FInal Commit For The Project. StepperV14

--- stepper14.py
+++ stepper14.py
@@ -17,7 +17,7 @@ CCW = 0
 SPR = 3200
 DEGREES_PER_REV = 360
 DEGREES_PER_STEP = DEGREES_PER_REV / SPR
-MAX_DEGREES = 20
+MAX_DEGREES = 25
 
 # Button setup (Can Be Modified)
 SPEED_UP_BTN = 2
@@ -26,7 +26,8 @@ DUTY_CYCLE_UP_BTN = 14
 DUTY_CYCLE_DOWN_BTN = 15
 START_BTN = 4
 
-speed = 0.00075  # Initial speed
+speed = 0.0008   # Initial speed
+wait_time = 0.8 # Inital Wait Time
 duty_cycle = 15  # Initial duty cycle
 
 # Setup MediaPipe Face Detection
@@ -40,7 +41,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 # Initialize PiCamera2
 piCam = Picamera2()
-piCam.preview_configuration.main.size=(640,480)
+piCam.preview_configuration.main.size=(720,480)
 piCam.preview_configuration.main.format="RGB888"
 piCam.preview_configuration.align()
 piCam.configure("preview")
@@ -50,7 +51,7 @@ face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([DIR, STEP, PWM_PIN, PWM_PIN2], GPIO.OUT)
-GPIO.setup(SOLENOID_PIN, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(SOLENOID_PIN, GPIO.OUT, initial = GPIO.HIGH)
 GPIO.setup([SPEED_UP_BTN, SPEED_DOWN_BTN, DUTY_CYCLE_UP_BTN, DUTY_CYCLE_DOWN_BTN, START_BTN], GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 dc_motor_pwm = GPIO.PWM(PWM_PIN, 490)    # PWM frequency 1000 Hz
@@ -79,13 +80,14 @@ def get_hand_position():
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
-                x, y = int(wrist.x * 600), int(wrist.y * 400)
-                return (x, y)
+                x, y = int(wrist.x * 760), int(wrist.y * 400)
+                print(x)
+                return x # POSSIBLE FIXXXXXXXXXXXXXXXXXXXXX
         else:
             return None
 
 def map_value_to_degrees(value):
-    return (value - 300) / ((600 - 0) / (MAX_DEGREES - (-MAX_DEGREES))) # Maps value from 0-1200 range to -90 to +90 degrees
+    return (value - 350) / ((700 - 0) / (MAX_DEGREES - (-MAX_DEGREES))) # Maps value from 0-1200 range to -90 to +90 degrees
 
 def rotate_stepper_motor(degrees, direction, speed):
     steps = int(abs(degrees) / DEGREES_PER_STEP)  # Calculate the number of steps
@@ -96,25 +98,29 @@ def rotate_stepper_motor(degrees, direction, speed):
         time.sleep(speed)
         GPIO.output(STEP, GPIO.LOW)
         time.sleep(speed)
-        f.write(f"{x}\n{direction}\n")  
 
 def control_dc_motor(duty_cycle):
     dc_motor_pwm.ChangeDutyCycle(duty_cycle)    # Set duty cycle
-    # time.sleep(2)                               # Hold for 2 seconds
+    # time.sleep(2)                             # Hold for 2 seconds
 
-def adjust_speed(increase = True):
+def adjust_speed(increase = True):  
     global speed
+    global wait_time
     if increase:
         speed = min(speed + 0.0001, 0.001)
+        wait_time = min(wait_time + 0.1, 1)
         print(f"Speed Increased {speed}")
+        print(f"Wait Increased {wait_time}")
     else:
         speed = max(speed - 0.0001, 0.0005)
+        wait_time = max(wait_time - 0.1, 0.5)
         print(f"Speed Decreased {speed}")
+        print(f"Wait Decreased {wait_time}")
 
 def adjust_duty_cycle(increase = True):
     global duty_cycle
     if increase:
-        duty_cycle = min(duty_cycle + 1, 25)
+        duty_cycle = min(duty_cycle + 1, 30)
         print(f"Duty Increase {duty_cycle}")
     else:
         duty_cycle = max(duty_cycle - 1, 5)
@@ -122,26 +128,8 @@ def adjust_duty_cycle(increase = True):
 
 def pulse_solenoid():
     GPIO.output(SOLENOID_PIN, GPIO.LOW)  # Activate solenoid
-    time.sleep(0.5)                      # Keep activated for 0.5 seconds
+    time.sleep(0.01)                      # Keep activated for 0.5 seconds
     GPIO.output(SOLENOID_PIN, GPIO.HIGH) # Deactivate solenoid
-
-def return_to_origin_if_exit():
-    with open("/home/pi/Desktop/ENEL400/StepMotorBackup.txt", "r") as f:
-        lines = f.read().splitlines()
-        last_direction = lines[-1]
-        last_step = lines[-2]
-
-    GPIO.output(DIR, CCW if last_direction == 'cw' else CW)
-    origin_fix = int(last_step)
-
-    for x in range(origin_fix):
-        GPIO.output(STEP, GPIO.HIGH)
-        time.sleep(0.001)
-        GPIO.output(STEP, GPIO.LOW)
-        time.sleep(0.001)
-
-    with open("/home/pi/Desktop/ENEL400/StepMotorBackup.txt", "w") as f:
-        f.write("")
 
 def run_program():
     global speed, duty_cycle
@@ -155,19 +143,15 @@ def run_program():
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Break loop if 'q' is pressed
             break
 
-        choices = ['face'] * 2 + ['hand'] * 3 + ['random'] * 5
+        choices = ['face'] * 5 + ['hand'] * 5
         action = random.choice(choices)
 
-        if action == 'face':
+        if (action == 'face'):
             value = get_face_position()
             print("Processing face position...")
-        elif action == 'hand':
-            # value = get_hand_position()
-            value = None
+        elif (action == 'hand'):
+            value = get_hand_position()
             print("Processing hand position...")
-        else:
-            value = random.randint(0, 600)
-            print("Performing a random action...")
 
         if value is not None:
             degrees = map_value_to_degrees(value)
@@ -181,6 +165,7 @@ def run_program():
             print(f"Rotating to {degrees} degrees {direction} with speed {speed:.4f}")
             rotate_stepper_motor(degrees, direction, speed)
             # time.sleep(1)
+            # time.sleep(0.1)
             pulse_solenoid()  # Pulse Solenoid
             # dc_motor_pwm.ChangeDutyCycle(0)  # Stop DC motor
             # time.sleep(2)
@@ -188,9 +173,8 @@ def run_program():
             # Return To Origin
             rotate_stepper_motor(degrees, 'ccw' if direction == 'cw' else 'cw', speed)
             print(f"Returning to origin from {degrees} degrees {direction} with speed {speed:.4f}")
-            f.write("0\ncw")
             print("Origin Reached")
-            time.sleep(1)
+            time.sleep(wait_time)
 
             face_detected_count += 1  # Increment the face detected counter
 
@@ -199,38 +183,41 @@ def run_program():
             # to prevent the program from running too fast and overwhelming the CPU
             time.sleep(0.1)
 
-# Initialize motor position by returning to origin
-f = open("/home/pi/Desktop/ENEL400/StepMotorBackup.txt", "r") 
-if (f.read() != ''):
-    f.close()
-    return_to_origin_if_exit()
-else:
-    f.close()
-
-f = open("/home/pi/Desktop/ENEL400/StepMotorBackup.txt", "w")
-
 try:
     while True:
         # Check button states
-        if not GPIO.input(SPEED_UP_BTN):
-            adjust_speed(increase = True)
-            time.sleep(1)
         if not GPIO.input(SPEED_DOWN_BTN):
+            adjust_speed(increase = True)
+            time.sleep(0.5)
+        if not GPIO.input(SPEED_UP_BTN):
             adjust_speed(increase = False)
-            time.sleep(1)
+            time.sleep(0.5)
         if not GPIO.input(DUTY_CYCLE_UP_BTN):
             adjust_duty_cycle(increase = True)
-            time.sleep(1)
+            time.sleep(0.5)
         if not GPIO.input(DUTY_CYCLE_DOWN_BTN):
             adjust_duty_cycle(increase = False)
-            time.sleep(1)
+            time.sleep(0.5)
         if not GPIO.input(START_BTN):
-            time.sleep(5)
-            run_program()
-            print("Completed 30 cycles of face detection and processing.")
-            dc_motor_pwm.ChangeDutyCycle(0)  # Stop DC motor
+            
+            start_time = time.time()
+            button_held = True  # Assume the button will be held down
+
+            while time.time() - start_time < 2:  # Check for 3 seconds
+                if GPIO.input(START_BTN):  # If button is released
+                    button_held = False
+                    break  # Exit the while loop if button is released
+                time.sleep(0.1)  # Short delay to prevent high CPU usage
+
+            if button_held:
+                time.sleep(3)
+                control_dc_motor(duty_cycle)
+                time.sleep(2)
+                run_program()
+                print("Completed 10 cycles of face detection and processing.")
+                dc_motor_pwm.ChangeDutyCycle(0)  # Stop DC motor
         time.sleep(0.1)
+        # GPIO.output(SOLENOID_PIN, GPIO.HIGH) # Deactivate solenoid
 
 finally:
-    f.close()
     piCam.stop()
